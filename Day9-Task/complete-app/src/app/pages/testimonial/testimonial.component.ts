ee310192ef6881a794d2762f1251fcc3e7645b4c
mohansurya11 <mohan270601@gmail.com>
2023-04-19 17:56:34 +0530

    Added Day9 Task

---
+++ Day9-Task/complete-app/src/app/pages/testimonial/testimonial.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { TestimonialService } from 'src/app/services/testimonial.service';
+
+@Component({
+  selector: 'app-testimonial',
+  templateUrl: './testimonial.component.html',
+  styleUrls: ['./testimonial.component.css']
+})
+export class TestimonialComponent {
+  feedbacklist:any;
+  constructor(private fs:TestimonialService){
+    this.fs.getFeedbacks().subscribe(
+      {
+        next: (data:any)=> this.feedbacklist=data,
+        error: ()=>this.feedbacklist=[]
+      }
+    )
+  }
+}
+
