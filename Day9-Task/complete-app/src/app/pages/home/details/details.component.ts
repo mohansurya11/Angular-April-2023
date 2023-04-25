6f7d938293dbb97ef0283accc298f1d452693e2f
mohansurya11 <mohan270601@gmail.com>
2023-04-25 22:17:10 +0530

    added details component in day9 task

---
+++ Day9-Task/complete-app/src/app/pages/home/details/details.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HomeService } from 'src/app/services/home.service';
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.css']
+})
+export class DetailsComponent {
+  id:any;
+  service:any;
+  details:any;
+  
+  constructor(private hs:HomeService,private ar:ActivatedRoute){
+    this.ar.params.subscribe(
+      {
+        next: (params)=>{
+          this.id=params['id']
+          this.readData()
+        },
+        error:()=>this.id=0
+
+      }
+      
+    )
+  }
+  readData(){
+    this.hs.getServiceById(this.id).subscribe({
+      next:(data:any)=>this.service=data,
+      error:()=>this.service={}
+    })
+
+    this.hs.getDetails(this.id).subscribe({
+      next: (data:any)=>this.details = data,
+      error:()=> this.details={}
+    })
+    }
+}
